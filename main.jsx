dd2dbd4f4db124a0d7dcdea6539ce2df0412328c
Angel Enahoro <132835278+Angel-Enahoro@users.noreply.github.com>
2024-04-10 19:52:11 +0200

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 //main
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./src/App.jsx";
+import App from "./src/App";
 import "./src/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
